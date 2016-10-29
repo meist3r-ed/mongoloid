24b350aa7623f251827c498bc9436e4d6c6c59d7
meist3r-ed <g.gaiax@gmail.com>
2016-10-29 02:08:32 -0200

    Initial commit

---
+++ src/mongoloid/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Mongoloid is a SQL to MongoDB conversor featuring
+ * the basic syntax for collection & document creation.
+ * For further documentation, please check README.MD.
+ */
+package mongoloid;
+
+/**
+ *
+ * @author meist3r-ed
+ */
+public class Main {
+    
+    private static void splash(){
+        System.out.println("    __  ___                        __      _     __");
+        System.out.println("   /  |/  /___  ____  ____ _____  / /___  (_)___/ /");
+        System.out.println("  / /|_/ / __ \\/ __ \\/ __ `/ __ \\/ / __ \\/ / __  / ");
+        System.out.println(" / /  / / /_/ / / / / /_/ / /_/ / / /_/ / / /_/ /  ");
+        System.out.println("/_/  /_/\\____/_/ /_/\\__, /\\____/_/\\____/_/\\__,_/   ");
+        System.out.println("                   /____/                      v0.0");
+        System.out.println(">>>>>  SQL 2 MongoDB Conversor by meist3r-ed  <<<<<");
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main.splash();
+    }
+    
+}
